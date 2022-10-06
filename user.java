fdac46e9166a3fd392bf921b33c40647faed7c6b
Marcin <71531928+marcin2x4@users.noreply.github.com>
2022-10-06 22:09:29 +0200

    Create user.java

---
+++ user.java
@@ -0,0 +1,19 @@
+package com.apporders;
+
+public class user {
+    
+    //Private access modifier
+    private String name;
+
+
+    //getter
+    public String getName() {
+        return this.name;
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
